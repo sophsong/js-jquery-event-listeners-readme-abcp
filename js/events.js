d56ed0431159fc2cba89c526d4551d6b0a24e772
Sophia Song <sophia.song@tufts.edu>
2018-01-21 18:11:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,8 @@
 //define functions here
-function= ("getIt")
+function getIt= ()
 $(getIt).ready(function(){alert("you sure you want to submit")
 
 //
 
-});
+}) ;return } alert ("you entered the wrong value") return; })
+
